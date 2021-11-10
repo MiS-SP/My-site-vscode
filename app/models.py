0d60a58c00622494f88e3de38ff50ad10434c5f5
MiS <MiSStartP@gmail.com>
2021-11-10 16:05:40 +0400

    2

--- app/models.py
+++ app/models.py
@@ -2,13 +2,10 @@
 Definition of models.
 """
 
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class pf_projs(models.Model):
-    title = models.CharField(max_length=60),
-    author = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-        )
-    body = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    body = models.TextField(null=True)
+    date_creation = models.DateField(auto_now_add=True)
