1ee48fe87450938e202c336fe26864cf7498e54b
MiS-SP <MiSstartP@gmail.com>
2021-11-18 08:02:18 +0400

    add btn toggle

---
+++ app/static/app/content/Pf_btn_toggler.js
@@ -0,0 +1,13 @@
+// Get the container element
+var btnContainer = document.getElementById("works-menu");
+
+var btns = btnContainer.getElementsByClassName("menu_btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
