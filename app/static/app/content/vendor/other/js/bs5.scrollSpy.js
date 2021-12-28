cc1132f7332d02318fe0972da7c418a184efd45c
MiS-SP <MiSstartP@gmail.com>
2021-12-28 11:17:23 +0400

    clean proj rep

--- app/static/app/content/vendor/other/js/bs5.scrollSpy.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('body').scrollspy({
-            target: '#navbar',
-            offset: 10
-        })
-    });
-</script>
